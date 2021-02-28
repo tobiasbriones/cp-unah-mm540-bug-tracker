d0c5094b147904b4b296e9535dc693f55db77267
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-28 17:36:43 -0600

    Init the QA page with a project select element

--- src/js/qa.mjs
+++ src/js/qa.mjs
@@ -1,3 +1,40 @@
 /*
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
+
+import { SoftwareProjectRepository } from './repository.mjs';
+
+const projectRepository = new SoftwareProjectRepository();
+
+init();
+
+function init() {
+  document.getElementById('projectSelect').addEventListener('change', onProjectSelectChange);
+  initProjectsSelect();
+}
+
+function initProjectsSelect() {
+  const projects = projectRepository.getAll();
+  const el = document.getElementById('projectSelect');
+
+  projects.forEach(project => {
+    const opEl = document.createElement('option');
+
+    opEl.setAttribute('value', project.code);
+    opEl.innerText = project.name;
+    el.appendChild(opEl);
+  });
+}
+
+function onProjectSelectChange() {
+  const el = document.getElementById('projectSelect');
+  const projectCode = parseInt(el.options[el.selectedIndex].value);
+
+  if (!isNaN(projectCode)) {
+    setProject(projectCode);
+  }
+}
+
+function setProject(projectCode) {
+
+}
