a41c73b0cc75f92df755d63dac7b8e7781daa0c6
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-28 19:03:55 -0600

    Add start page options to go to the other pages (admin, dev and qa)

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,3 +1,14 @@
 /*
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
+
+init();
+
+function init() {
+  document.querySelectorAll('.options > div')
+          .forEach(el => el.addEventListener('click', onOptionClick));
+}
+
+function onOptionClick(e) {
+  window.location = e.target.dataset.page;
+}
