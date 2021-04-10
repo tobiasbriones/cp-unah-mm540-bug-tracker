f4a72cf431a7a0157c860f4e659219844f6921c2
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-10 16:37:09 -0600

    Add empty methods

--- server/src/http/admin/admin.service.mjs
+++ server/src/http/admin/admin.service.mjs
@@ -2,3 +2,78 @@
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
 
+export class AdminService {
+  constructor() {
+  }
+
+  async assignBug(bug, devTeam) {
+
+    
+  }
+
+  async readAllBugStats() {
+
+  }
+
+  async createDevTeam(devTeam) {
+
+    
+  }
+
+  async readAllDevTeams() {
+
+    
+  }
+
+  async readDevTeam(id) {
+
+  }
+
+  async updateDevTeam(id, devTeam) {
+
+  }
+
+  async deleteDevTeam(id) {
+
+  }
+
+  async createProject(project) {
+
+  }
+
+  async readAllProjects() {
+
+  }
+
+  async readProject(id) {
+
+  }
+
+  async updateProject(id) {
+
+  }
+
+  async deleteProject(id) {
+
+  }
+
+  async createUser(user) {
+
+  }
+
+  async readAllUsers() {
+
+  }
+
+  async readUser(id) {
+
+  }
+
+  async updateUser(id, user) {
+
+  }
+
+  async deleteUser(id) {
+
+  }
+}
