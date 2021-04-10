cff603c5258446dd86449fa1b7c033398445caa4
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-10 17:23:58 -0600

    Define UserModel

--- server/src/database/user.model.mjs
+++ server/src/database/user.model.mjs
@@ -2,3 +2,14 @@
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
 
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  id_usuario: { type: Number, unique: true },
+  nombre_completo: { type: String },
+  login: { type: String, unique: true },
+  password: [String],
+  rol: ['admin', 'dev', 'qa']
+});
+export const UserModel = mongoose.model('UserModel', UserSchema);
