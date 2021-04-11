d45b4a7a3aa04529b559319052aee1237983c6c1
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-10 23:27:37 -0600

    Implement AuthService

---
+++ client/src/js/auth.service.mjs
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ */
+
+import { BASE_URL } from './app.config.mjs';
+import axios from 'axios';
+
+const BASE_URL = BASE_URL + '/auth';
+const UAT_KEY = 'uat';
+
+export class AuthService {
+  constructor() {
+  }
+
+  getLogin() {
+    return Cookies.get(UAT_KEY);
+  }
+
+  saveLogin(jwt) {
+    Cookies.set(UAT_KEY, jwt);
+  }
+
+  deleteLogin() {
+    Cookies.remote(UAT_KEY);
+  }
+
+  async authenticate(login, password) {
+    const url = BASE_URL + '/login';
+    const data = { login, password };
+    return await axios.post(url, data);
+  }
+}
--- client/src/js/users.service.mjs
+++
@@ -1,4 +0,0 @@
-/*
- * Copyright (c) 2021 Tobias Briones. All rights reserved.
- */
-
