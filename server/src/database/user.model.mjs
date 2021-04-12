2d1fa4dfce63150af310d9c184918cacbeae4c3a
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-11 21:37:19 -0600

    Add require option

--- server/src/database/user.model.mjs
+++ server/src/database/user.model.mjs
@@ -7,13 +7,14 @@ import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  id_usuario: { type: Number, unique: true },
-  nombre_completo: { type: String },
-  login: { type: String, unique: true },
-  password: String,
+  id_usuario: { type: Number, unique: true, required: true },
+  nombre_completo: { type: String, required: true },
+  login: { type: String, unique: true, required: true },
+  password: {type: String, required: true},
   rol: {
     type: String,
-    enum: ['admin', 'dev', 'qa']
+    enum: ['admin', 'dev', 'qa'],
+    required: true
   }
 });
 
