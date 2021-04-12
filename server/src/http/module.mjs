3debd8754f24c644ef064d4effed0505c7c1b053
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-12 13:45:27 -0600

    Implement Module
    This base class is going to be useful for managing http modules (children modules for now).

--- server/src/http/module.mjs
+++ server/src/http/module.mjs
@@ -2,3 +2,28 @@
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
 
+import express from 'express';
+
+export class Module {
+  #path;
+  #middlewares;
+  #router;
+
+  constructor({ path = '/', middlewares = [] }) {
+    this.#path = path;
+    this.#middlewares = middlewares;
+    this.#router = express.Router();
+  }
+
+  get path() {
+    return this.#path;
+  }
+
+  get middlewares() {
+    return this.#middlewares;
+  }
+
+  get router() {
+    return this.#router;
+  }
+}
