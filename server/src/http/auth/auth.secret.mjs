9a593799bc5f31e768ae0ba064ca76d407c3fef7
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-12 14:10:34 -0600

    Add constant: JWT_PRIVATE_KEY

--- server/src/http/auth/auth.secret.mjs
+++ server/src/http/auth/auth.secret.mjs
@@ -2,3 +2,6 @@
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
 
+// This is a testing module for holding secret keys
+
+export const JWT_PRIVATE_KEY = ':D'; // Save it into a safe place
