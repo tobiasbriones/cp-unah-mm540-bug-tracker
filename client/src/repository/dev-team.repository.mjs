8604602d938ad838dd08c4cc4417c47a97e9d567
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-11 22:02:14 -0600

    Add DevTeamRepository

---
+++ client/src/repository/dev-team.repository.mjs
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ */
+
+import axios from 'axios';
+import { API_BASE_URL } from '../app.config.mjs';
+import { AuthService } from '../ui/auth/auth.service.mjs';
+
+const BASE_URL = API_BASE_URL + '/admin/dev-team';
+
+export class DevTeamRepository {
+  constructor() {
+    this.authService = new AuthService();
+  }
+
+  async getAll() {
+    const login = this.authService.getLogin();
+    const config = {
+      headers: { Authorization: `Bearer ${ login.uat }` }
+    };
+    const res = await axios.get(BASE_URL, config);
+    return res.data;
+  }
+
+  async get(id) {
+    const login = this.authService.getLogin();
+    const config = {
+      headers: { Authorization: `Bearer ${ login.uat }` }
+    };
+    const url = `${ BASE_URL }/${ id }`;
+    const res = await axios.get(url, config);
+    return res.data;
+  }
+
+  async add(devTeam) {
+    const login = this.authService.getLogin();
+    const config = {
+      headers: { Authorization: `Bearer ${ login.uat }` }
+    };
+    await axios.post(BASE_URL, devTeam, config);
+  }
+
+  async set(devTeam) {
+    const login = this.authService.getLogin();
+    const config = {
+      headers: { Authorization: `Bearer ${ login.uat }` }
+    };
+    const url = `${ BASE_URL }/${ devTeam.id_usuario }`;
+    await axios.put(url, devTeam, config);
+  }
+
+  async remove(id) {
+    const login = this.authService.getLogin();
+    const config = {
+      headers: { Authorization: `Bearer ${ login.uat }` }
+    };
+    const url = `${ BASE_URL }/${ id }`;
+    await axios.delete(url, config);
+  }
+}
