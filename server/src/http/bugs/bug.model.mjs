3a63f887e3701f3c18c3def34ee51f8def850553
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-12 22:19:45 -0600

    Update BugModel

--- server/src/http/bugs/bug.model.mjs
+++ server/src/http/bugs/bug.model.mjs
@@ -8,7 +8,6 @@ const Schema = mongoose.Schema;
 const BugSchema = new Schema({
   code: {type: Number, unique: true},
   description: String,
-  swProjectCode: Number,
   priority: { type: Number, min: 1, max: 5 },
   state: {
     type: String,
