4f8599e815d935859ed6964d90563d690f668ca1
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-04-12 21:35:48 -0600

    Define TeamProjectModel

--- server/src/http/teams/team-project.model.mjs
+++ server/src/http/teams/team-project.model.mjs
@@ -2,3 +2,11 @@
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
 
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const TeamProjectSchema = new Schema({
+  teamCode: { type: Number },
+  projectCode: { type: Number },
+});
+export const TeamProjectModel = mongoose.model('TeamProject', TeamProjectSchema);
