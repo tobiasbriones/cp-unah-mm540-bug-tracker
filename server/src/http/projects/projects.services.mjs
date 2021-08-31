e0e1ea5f8ad1b54d842d8faa2ee5f1ab8140c2d6
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-08-30 23:41:46 -0600

    Update copyright license header

--- server/src/http/projects/projects.services.mjs
+++ server/src/http/projects/projects.services.mjs
@@ -1,5 +1,13 @@
 /*
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM540: Bug Tracker.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
  */
 
 import { ProjectModel } from './project.model.mjs';
