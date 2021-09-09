dcd2d05e80f95f58aa143ae62ee186b7937ebf3f
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-09-08 23:23:29 -0600

    Update component: Teams

--- client/src/ui/admin/teams/Teams.js
+++ client/src/ui/admin/teams/Teams.js
@@ -13,7 +13,8 @@
 import './Teams.css';
 import React from 'react';
 import { TeamRepository } from '../../../model/team/team.repository.mjs';
-import { getCreateForm, getTable, getUpdateForm } from '../../../crud';
+import Crud from '../../../crud/Crud';
+import { getErrorMessage } from '../../../crud/errors';
 
 class Teams extends React.Component {
   constructor(props) {
@@ -21,208 +22,140 @@ class Teams extends React.Component {
     this.state = {
       displayClass: '',
       values: [],
-      createFormDisplayClass: 'd-none',
-      updateFormDisplayClass: 'd-none',
       createError: '',
-      createName: '',
-      createLanguages: '',
-      createTech: '',
-      updateError: '',
-      updateCode: '',
-      updateName: '',
-      updateLanguages: '',
-      updateTech: ''
+      updateError: ''
     };
     this.teamRepository = new TeamRepository();
+    this.crudRef = React.createRef();
   }
 
   render() {
-    const getTeamRow = team => {
-      return (
-        <tr key={ team.code } data-key={ team.code } onClick={ this.onTeamRowClick.bind(this) }>
-          <th scope="col">{ team.code }</th>
-          <th scope="col">{ team.name }</th>
-          <th scope="col">{ team.languages }</th>
-          <th scope="col">{ team.tech }</th>
-        </tr>
-      );
+    const commonInputs = [
+      {
+        name: 'name',
+        type: 'text',
+        label: 'Nombre',
+        placeholder: 'Nombre del equipo',
+        value: ''
+      },
+      {
+        name: 'languages',
+        type: 'text',
+        label: 'Lenguajes de programación',
+        placeholder: 'Lenguajes de programación',
+        smallText: 'Ingresa cada item separado por comas',
+        value: ''
+      },
+      {
+        name: 'tech',
+        type: 'text',
+        label: 'Tecnologías',
+        placeholder: 'Tecnologías',
+        smallText: 'Ingresa cada item separado por comas',
+        value: ''
+      }
+    ];
+    const readAllTable = {
+      cols: [
+        {
+          name: 'code',
+          value: 'Código'
+        },
+        {
+          name: 'name',
+          value: 'Nombre'
+        },
+        {
+          name: 'languages',
+          value: 'Lenguajes'
+        },
+        {
+          name: 'tech',
+          value: 'Tecnologías'
+        }
+      ],
+      items: this.state.values
+    };
+    const createForm = {
+      title: 'Crear Equipo',
+      error: this.state.createError,
+      inputs: [...commonInputs]
+    };
+    const updateForm = {
+      title: 'Actualizar Equipo',
+      error: this.state.updateError,
+      inputs: [
+        {
+          isId: true,
+          name: 'code',
+          type: 'number',
+          label: 'Código',
+          value: -1
+        },
+        ...commonInputs
+      ]
     };
 
-    const getCreateGroups = () => {
-      return [
-        (
-          <div className="form-group" key="createName">
-            <label htmlFor="userCreateNameInput">Nombre</label>
-            <input id="userCreateNameInput"
-                   className="form-control"
-                   type="text"
-                   placeholder="Nombre"
-                   onChange={ this.onCreateNameChange.bind(this) } />
-          </div>
-        ),
-        (
-          <div className="form-group" key="createLogin">
-            <label htmlFor="userCreateLoginInput">Languages</label>
-            <input id="userCreateLoginInput"
-                   className="form-control"
-                   type="text"
-                   placeholder="Languages"
-                   onChange={ this.onCreateLanguagesChange.bind(this) } />
-          </div>
-        ),
-        (
-          <div className="form-group" key="createLogin">
-            <label htmlFor="userCreateLoginInput">Tecnologías</label>
-            <input id="userCreateLoginInput"
-                   className="form-control"
-                   type="text"
-                   placeholder="Lenguajes"
-                   onChange={ this.onCreateTechChange.bind(this) } />
-          </div>
-        )
-      ];
-    };
-
-    const getUpdateGroups = () => {
-      return [
-        (
-          <div className="form-group" key="updateId">
-            <label htmlFor="userUpdateIdInput">Código</label>
-            <input id="userUpdateIdInput"
-                   className="form-control"
-                   type="number"
-                   disabled
-                   aria-describedby="userUpdateIdHelp"
-                   value={ this.state.updateCode } />
-          </div>
-        ),
-        (
-          <div className="form-group" key="updateName">
-            <label htmlFor="userUpdateNameInput">Nombre</label>
-            <input id="userUpdateNameInput"
-                   className="form-control"
-                   type="text"
-                   placeholder="Nombre"
-                   value={ this.state.updateName }
-                   onChange={ this.onUpdateNameChange.bind(this) } />
-          </div>
-        ),
-        (
-          <div className="form-group" key="updateLogin">
-            <label htmlFor="userUpdateLoginInput">Lenguajes</label>
-            <input id="userUpdateLoginInput" className="form-control" type="text"
-                   placeholder="Lenguajes"
-                   value={ this.state.updateLanguages }
-                   onChange={ this.onUpdateLanguagesChange.bind(this) } />
-          </div>
-        ),
-        (
-          <div className="form-group" key="updateLogin">
-            <label htmlFor="userUpdateLoginInput">Tecnologías</label>
-            <input id="userUpdateLoginInput" className="form-control" type="text"
-                   placeholder="Lenguajes"
-                   value={ this.state.updateTech }
-                   onChange={ this.onUpdateTechChange.bind(this) } />
-          </div>
-        )
-      ];
-    };
-
-    const teams = getTable({
-      ctx: this,
-      title: 'Equipos de Desarrollo',
-      cols: ['Código', 'Nombre', 'Lenguajes', 'Tecnologías'],
-      getRow: getTeamRow
-    });
-    const create = getCreateForm({
-      ctx: this,
-      title: 'Crear equipo',
-      groups: getCreateGroups()
-    });
-    const update = getUpdateForm({
-      ctx: this,
-      title: 'Actualizar equipo',
-      groups: getUpdateGroups()
-    });
     return (
       <div className={ `row page ${ this.state.displayClass }` }>
-        { teams }
-        { create }
-        { update }
+        <Crud
+          title="Desarrolladores"
+          readAllTable={ readAllTable }
+          createForm={ createForm }
+          updateForm={ updateForm }
+          onCreate={ this.onCreate.bind(this) }
+          onUpdate={ this.onUpdate.bind(this) }
+          onDelete={ this.onDelete.bind(this) }
+          ref={ this.crudRef }
+        />
       </div>
     );
   }
 
-  async onPageShowed() {
+  async componentDidMount() {
     await this.loadTeams();
   }
 
-  onAddButtonClick() {
-    const value = this.state.createFormDisplayClass ? '' : 'd-none';
-    this.setState({ createFormDisplayClass: value, updateFormDisplayClass: 'd-none' });
+  async onCreate(team) {
+    this.fix(team);
+
+    try {
+      await this.teamRepository.add(team);
+      await this.loadTeams();
+      this.crudRef.current.collapse();
+    }
+    catch (e) {
+      const msg = getErrorMessage(e);
+      this.setState({ createError: msg });
+    }
   }
 
-  async onTeamRowClick(e) {
-    const id = e.target.parentElement.getAttribute('data-key');
-    const team = await this.teamRepository.get(id);
-    this.setState({
-      updateFormDisplayClass: '',
-      createFormDisplayClass: 'd-none',
-      updateCode: team.code,
-      updateName: team.name,
-      updateLanguages: team.languages,
-      updateTech: team.tech
-    });
+  async onUpdate(team) {
+    this.fix(team);
+
+    try {
+      await this.teamRepository.set(team);
+      await this.loadTeams();
+      this.crudRef.current.collapse();
+    }
+    catch (e) {
+      const msg = getErrorMessage(e);
+      this.setState({ updateError: msg });
+    }
   }
 
-  onCreateNameChange(e) {
-    this.setState({ createName: e.target.value });
-  }
+  async onDelete(team) {
+    const { code } = team;
 
-  onCreateLanguagesChange(e) {
-    this.setState({ createLanguages: e.target.value });
-  }
-
-  onCreateTechChange(e) {
-    this.setState({ createTech: e.target.value });
-  }
-
-  onUpdateNameChange(e) {
-    this.setState({ updateName: e.target.value });
-  }
-
-  onUpdateLanguagesChange(e) {
-    this.setState({ updateLanguages: e.target.value });
-  }
-
-  onUpdateTechChange(e) {
-    this.setState({ updateTech: e.target.value });
-  }
-
-  async onCreateFormSubmit(e) {
-    e.preventDefault();
-    const team = {
-      name: this.state.createName,
-      languages: this.state.createLanguages,
-      tech: this.state.createTech
-    };
-    await this.createTeam(team);
-  }
-
-  async onUpdateFormSubmit(e) {
-    e.preventDefault();
-    const team = {
-      code: this.state.updateCode,
-      name: this.state.updateName,
-      languages: this.state.updateLanguages,
-      tech: this.state.updateTech
-    };
-    await this.updateTeam(team);
-  }
-
-  async onDelete() {
-    const code = this.state.updateCode;
-    await this.deleteTeam(code);
+    try {
+      await this.teamRepository.remove(code);
+      await this.loadTeams();
+      this.crudRef.current.collapse();
+    }
+    catch (e) {
+      const msg = getErrorMessage(e);
+      this.setState({ updateError: msg });
+    }
   }
 
   async loadTeams() {
@@ -230,49 +163,29 @@ class Teams extends React.Component {
       const teams = await this.teamRepository.getAll();
       this.setState({
         values: teams,
-        createFormDisplayClass: 'd-none',
-        updateFormDisplayClass: 'd-none',
         createError: '',
         updateError: ''
       });
     }
     catch (e) {
-      const msg = e.response.data ? e.response.data : e;
+      const msg = getErrorMessage(e);
       alert(msg);
     }
   }
 
-  async createTeam(team) {
-    try {
-      await this.teamRepository.add(team);
-      await this.loadTeams();
+  fix(team) {
+    if (
+      !team.languages ||
+      !team.languages.split ||
+      !team.tech ||
+      !team.tech.split
+    ) {
+      return;
     }
-    catch (e) {
-      const msg = e.response.data ? e.response.data : e;
-      this.setState({ createError: msg });
-    }
-  }
-
-  async updateTeam(team) {
-    try {
-      await this.teamRepository.set(team);
-      await this.loadTeams();
-    }
-    catch (e) {
-      const msg = e.response.data ? e.response.data : e;
-      this.setState({ updateError: msg });
-    }
-  }
-
-  async deleteTeam(id) {
-    try {
-      await this.teamRepository.remove(id);
-      await this.loadTeams();
-    }
-    catch (e) {
-      const msg = e.response.data ? e.response.data : e;
-      this.setState({ updateError: msg });
-    }
+    const languages = team.languages.split(',').map(s => s.trim());
+    const tech = team.tech.split(',').map(s => s.trim());
+    team.languages = languages;
+    team.tech = tech;
   }
 }
 
