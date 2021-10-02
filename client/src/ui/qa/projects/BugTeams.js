45205f869e27cf097177b357eabd9e4e98cd2d0b
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-01 23:50:54 -0600

    Migrate component: BugTeams

---
+++ client/src/ui/qa/projects/BugTeams.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM540: Bug Tracker.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+import React from 'react';
+
+class BugTeams extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const mapItem = item =>
+      <li key={ item.code } className="list-group-item">{ item.name }</li>;
+
+    if (this.props.projectCode === -1) {
+      return <div />;
+    }
+    return (
+      <div className="col-0 col-xl-4 mt-5 mt-xl-0 align-self-center">
+        <p className="text-center font-weight-bold">
+          <strong>Desarrolladores asignados</strong>
+        </p>
+        <ul className="list-group">
+          { this.props.items.map(mapItem) }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BugTeams;
