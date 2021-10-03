c5f076893d998cbab0a7d4b33317016564641e45
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-02 23:41:40 -0600

    Migrate Dev component: TeamBug

---
+++ client/src/ui/dev/TeamBugs.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM540: Bug Tracker.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+import React from 'react';
+
+class TeamBugs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const mapBug = bug => (
+      <label className="list-group-item  list-group-item-primary" key={ bug.code }>
+        <input className="form-check-input me-1" type="checkbox" value={ bug.code } />
+        #{ bug.code } - { bug.description }
+      </label>
+    );
+
+    return (
+      <div>
+        <p className="text-center font-weight-bold">
+          <strong>Bugs asignados</strong>
+        </p>
+        <div className="list-group">
+          { this.props.values.map(mapBug) }
+        </div>
+      </div>
+    );
+  }
+
+  onChange(e) {
+    if (this.props.onChange) {
+      const value = e.target.value;
+      console.log(value);
+      this.props.onChange(value);
+    }
+  }
+}
+
+export default TeamBugs;
+
+TeamBugs.defaultProps = {
+  values: []
+};
