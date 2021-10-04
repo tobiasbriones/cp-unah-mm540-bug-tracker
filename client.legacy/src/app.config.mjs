92a736a2a758acdfc0a890918c307eed0beec6a8
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-03 18:57:08 -0600

    Delete project: ./client.legacy
    The client app was completely migrated to React and there are only some bugs to fix which don't need any legacy code or information to move on.

--- client.legacy/src/app.config.mjs
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2021 Tobias Briones. All rights reserved.
- */
-
-export const API_BASE_URL = 'http://localhost:3000';
--- client.legacy/src/ui/admin/table.mjs
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021 Tobias Briones. All rights reserved.
- */
-
-export function setSelected(rowEl) {
-  document.querySelectorAll('table tr.selected')
-          .forEach(rowEl => rowEl.classList.remove('selected'));
-  rowEl.classList.add('selected');
-}
